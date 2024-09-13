2cca5603d566ec5fabe3ba34bbf8ce7153695173
n0g4y0 <42777385+n0g4y0@users.noreply.github.com>
2024-09-12 23:16:16 -0400

    add global constants

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+
+const $h1 = $("h1");
+const $button = $("#btn-show");
+const $select = $("#select-base");
+const $btnShow = document.querySelector("#btn-show");
+const $btnShowCoins = document.querySelector("#btn-show-coins");
+const $inputDate = $("#date");
+const URL = 'https://api.frankfurter.app'
+const $CURRENCIES = ["AUD","BGN","BRL","CAD","CHF","CNY","CZK","DKK","GBP","HKD","HUF","IDR","ILS","INR","ISK","JPY","KRW","MXN","MYR","NOK","NZD","PHP","PLN","RON","SEK","SGD","THB","TRY","USD","ZAR"];
+
+
+
